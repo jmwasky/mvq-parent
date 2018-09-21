2d493aa456fdf44d0320de3c8272f74d877542c3
jmwasky <964692548@qq.com>
2018-09-21 18:03:52 +0800

    first code

---
+++ mvq-front-end-jsp/src/main/java/com/shsnc/mvq/FrontEndApplication.java
@@ -0,0 +1,20 @@
+package com.shsnc.mvq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liangchao
+ * @data on 2018/03/27
+ */
+@SpringBootApplication
+public class FrontEndApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontEndApplication.class, args);
+	}
+
+
+
+}
